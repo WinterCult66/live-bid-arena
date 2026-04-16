72382d3735b4971a31cfbc6b04b16c3ed9757719
Kevin Rodriguez <KARODRIG@proteccion.com.co>
2026-04-16 02:39:34 -0500

    implement table view v2

--- src/app/features/auction/components/bid-console/bid-console.ts
+++ src/app/features/auction/components/bid-console/bid-console.ts
@@ -1,15 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-bid-console',
   standalone: true,
-  template: `
-    <div>
-      <button (click)="bid.emit(10)">+10</button>
-      <button (click)="bid.emit(50)">+50</button>
-      <button (click)="bid.emit(100)">+100</button>
-    </div>
-  `
+  templateUrl: './bid-console.html',
+  styleUrl: './bid-console.css'
 })
 export class BidConsoleComponent {
   @Output() bid = new EventEmitter<number>();
